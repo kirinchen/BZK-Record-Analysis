098f1f5966ccbb0e0b3d6578239a32792574bdc3
kirin <kirin.chen1001@gmail.com>
2020-02-02 10:53:29 +0800

    can run the jest unit test

--- test/base.test.ts
+++ test/base.test.ts
@@ -15,6 +15,13 @@ describe("Test Suite 1", () => {
 
 });*/
 
-test('Base Test', () => {
-    expect(3).toBe(3);
+test('CryptoSymbol Test', () => {
+    let bc = CryptoSymbol.fullName(CPSymbol.BTC);
+    expect(bc).toBe( "bitcoin");
+    let sd = new Date(2017, 0, 1);
+    let ed = new Date(2018, 10, 1);
+    let cmcurl = CryptoSymbol.historicalUrl(CPSymbol.BTC, sd, ed);
+    console.log("cmcurl:" + cmcurl);
+    console.log("sd:" + sd + " ed:" + ed);
+    expect(cmcurl).toBe("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20170101&end=20181101");
 });
